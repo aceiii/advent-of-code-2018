cdbf09a642b70530c1e9851bad654a657845847e
Borin Ouch <borin.ouch@gmail.com>
2026-02-16 22:07:56 -0800

    Implement day16 part 2

--- day16.py
+++ day16.py
@@ -10,13 +10,17 @@ def parse_samples(lines):
     return { "op": op, "before": before, "after": after }
 
 
+def parse_instrs(line):
+    return [int(a, 10) for a in line.split(' ')]
+
+
 def parse(lines):
     prev = []
     samples = []
-    test = []
+    instrs = []
     blank = 0
     mode = 0
-    for line in lines:
+    for idx,line in enumerate(lines):
         if mode == 0:
             if line.strip() == '':
                 blank += 1
@@ -28,12 +32,11 @@ def parse(lines):
                 samples.append(prev)
                 prev = []
             elif blank == 3:
-                mode == 1
+                mode = 1
                 blank = 0
-                continue
         else:
-            samples.append(line.strip())
-    return [parse_samples(i) for i in samples], test
+            instrs.append(line.strip())
+    return [parse_samples(i) for i in samples], [parse_instrs(i) for i in instrs]
 
 
 def addr(regs, a, b, c):
@@ -85,7 +88,6 @@ def eqrr(regs, a, b, c):
     regs[c] = 1 if regs[a] == regs[b] else 0
 
 
-
 all_ops = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
 
 def test_sample(sample):
@@ -100,7 +102,7 @@ def test_sample(sample):
 
 
 def part1(lines):
-    samples, test = parse(lines)
+    samples, _ = parse(lines)
     answer = 0
     for sample in samples:
         if test_sample(sample) >= 3:
@@ -108,8 +110,46 @@ def part1(lines):
     return answer
 
 
+def solve_ops(samples):
+    ops_map = {k:[1] * 16 for k in range(16)}
+    for sample in samples:
+        for idx, op_func in enumerate(all_ops):
+            regs = sample["before"][:]
+            op_id, a, b, c = sample["op"]
+            op_func(regs, a, b, c)
+            if regs != sample["after"]:
+                ops_map[op_id][idx] = 0
+
+    ops = list(ops_map.items())
+    known_ops = {}
+    used_ops = set()
+
+    while ops:
+        ops.sort(key=lambda a: sum(a[1]), reverse=True)
+        while ops and sum(ops[-1][1]) == 1:
+            k,v = ops.pop()
+            op_idx = v.index(1)
+            known_ops[k] = op_idx
+            used_ops.add(op_idx)
+        for op in ops:
+            for i in range(16):
+                if i in used_ops:
+                    op[1][i] = 0
+    return known_ops
+
+
+def run(regs, ops, instrs):
+    for op_id, a, b, c in instrs:
+        op_func = all_ops[ops[op_id]]
+        op_func(regs, a, b, c)
+
+
 def part2(lines):
-    pass
+    samples, instrs = parse(lines)
+    ops = solve_ops(samples)
+    regs = [0, 0, 0, 0]
+    run(regs, ops, instrs)
+    return regs[0]
 
 
 def main():
